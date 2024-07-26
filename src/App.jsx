f1b6f37e81f52125fe8461b6045ac62363c74bb2
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-07-26 18:30:01 +0200

    Clear code in App component by old code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { FocusableInput } from "./FocusableInput";
 
 export function App() {
-  function handleLogin(state) {
-    console.log(`Lo stato del login è ${state}`);
-  }
   return (
     <div>
       <h1>My Awesome Application</h1>
