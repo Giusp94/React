654fc8f846de2632e42270b751d029fce95794c0
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-08-05 12:07:57 +0200

    Change language to h4-Select language

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ export function App() {
     <LanguageContext.Provider value={lang}>
       <h1>My Awesome Application</h1>
       <hr />
-      <h4>Select Language: </h4>
+      {lang === "en" ? (
+        <h4>Select Language: </h4>
+      ) : (
+        <h4>Seleziona la lingua: </h4>
+      )}
       <select name="language" id="language" onChange={handleChangeLanguage}>
         <option value="en">EN</option>
         <option value="it">IT</option>
